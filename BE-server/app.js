31d2031d0adf338fd0a7cb8841c88de8723b55e4
Boddah98 <andy.acros@estudiantec.cr>
2024-02-21 15:14:22 -0600

    Backend server and controller creation
    waiting for the connection

---
+++ BE-server/app.js
@@ -0,0 +1,31 @@
+// server object for backend
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+const cors = require('cors');
+app.use(cors());
+//Port define
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+// Words data methods and const
+
+const wordsController = require('./controllers/words');
+app.get("/getWordSet",wordsController.getWordsSet);
+
+// Results data methods and const
+
+
+/* Controllers for api calls
+const questionController = require('./controllers/questions');
+
+file system module, decode type and request function 
+app.get("/getQuestionSet",questionController.getQuestionSet);
+*/
+
+
+module.exports = app;
---
+++ BE-server/controllers/words.js
@@ -0,0 +1,43 @@
+// file system and path libs for manage the words files
+const { randomInt } = require('crypto');
+const fs = require('fs');
+const path = require ('path');
+const wordsFilePath = path.join(__dirname,'../../data/words.json');
+
+module.exports = class Controller{
+    
+    static getWordsSet = async (req, res) => {        
+        fs.readFile(wordsFilePath, 'utf8',(err, data)=>{
+            if(err){
+                console.error('Error de lectura de datos',err);
+                return;
+            }
+            const jsonWordsData = JSON.parse(data);
+            const fourWords = this.getFourPack(jsonWordsData);
+            return res.send({
+                code : 200,
+                data : fourWords
+            });
+        });
+    }
+    // The list of the words select must not have the same options 
+    static getFourPack(jsondata){
+        let indexList = [];
+        let wordList = [];
+        while (indexList.length<4){
+            let wordIndex = randomInt(79);
+            if(wordIndex in indexList){
+                console.log(wordIndex," is in indexList", indexList);
+                continue;
+            }else{                
+                indexList.push(wordIndex);
+                wordList.push(jsondata[wordIndex])
+                //console.log("word",jsondata[wordIndex]," added to 4pack. New words list:");
+                //console.log(wordList);
+
+            }
+        }
+        return wordList;
+       
+    }
+}
