bc8d6dd85153b9269ba08ea32151f3b9f34220b5
Boddah98 <andy.acros@estudiantec.cr>
2024-02-24 16:06:49 -0600

    keys working
    next step is setting the game status

--- FE-app/src/app/game.component/game.component.ts
+++ FE-app/src/app/game.component/game.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import{ ConnectionService } from '../services/connection';
 import { firstValueFrom } from 'rxjs';
 import { word } from '../models/word';
+
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
@@ -15,20 +16,20 @@ export class gameComponent implements OnInit{
     "Z","X","C","V","B","N","M"    
   ];
   cs : ConnectionService;
-  displayPopup = false;
+  displayPopup = true;
   input1='';
   input2='';
   player1 ='';
   player2 ='';
   words : word[];
+  actualWord = '';
+  round = 1;
+  wordIndex=0;
+  hangedStatus=0;
   constructor(cs: ConnectionService){
     this.cs = cs;
     this.words= [];
-    document.addEventListener('DOMContentLoaded', () => {
-      this.createKeyboard();
-      this.togglePopup();
-      this.newGame();
-    });
+    
 
   }
   ngOnInit(): void {
@@ -54,22 +55,64 @@ export class gameComponent implements OnInit{
     }
     
   }
-  newGame(){
+  /*
+  * Create all the enviroment for start a game 
+  */
+  async newGame(){
+    // gets the words from the backend
+    this.words = (await firstValueFrom(this.cs.getWords())).data as word[];
+    this.actualWord = (this.words[0]).word;
+    console.log(this.actualWord)
     this.randPlayerSelect();
     this.togglePopup();
-    this.setHangedStatus();
-    this.getWordSet();
+    this.createKeyboard();     
+    this.setHangedStatus();    
+    this.setWordLetters()
   }
+  /*
+  * charges the letters container with hidden letter cards for the game using 
+  * the connection service gets 4 random words from the backend before
+  */
+  async setWordLetters(){
+    const lettersContainer = document.querySelector("#lettersContainer") as HTMLElement | null;  
+    const wordObj: word | undefined = this.words[this.wordIndex];
+    let word = wordObj.word;
+    for(let i=0;i<word.length;i++){
+      let letter = word[i];
+      let gameLetter = document.createElement("mat-card")
+      let labelLetter = document.createElement("strong")
+      labelLetter.setAttribute("id","gl-"+i+letter);
+      labelLetter.innerHTML = "_";
+      gameLetter.appendChild(labelLetter);
+      lettersContainer?.appendChild(gameLetter)
+    }        
+  }
+  
+  /*
+  * actualizes the image instead of the status of the hanged man 
+  */
   setHangedStatus(){
     const hangedField = document.getElementById("hangedField")      
     let imgGame = document.createElement("img");
-    imgGame.setAttribute("src","../../assets/ah6.png");
+    imgGame.setAttribute("id", "hangedImg" )
+    imgGame.setAttribute("src","../../assets/ah0.png");
     hangedField?.appendChild(imgGame);
   }
-  async getWordSet(){
-    this.words = (await firstValueFrom(this.cs.getWords())).data as word[];
-    console.log(this.words)
+  updateHangedStatus(){
+    if (this.hangedStatus  > 6){
+      console.log("game is over dude")
+      return 
+    }
+    let imgGame = document.getElementById("hangedImg") as HTMLElement
+    console.log("../../assets/ah"+this.hangedStatus+".png")
+    imgGame.setAttribute("src","../../assets/ah"+this.hangedStatus+".png")
+    if (this.hangedStatus == 6){
+      console.log("game over")
+    }
   }
+  /*
+  * Chooses the first turn with the input names of the popup
+  */
   randPlayerSelect(){
     let num = Math.floor(Math.random()*2);
     if (num==1){
@@ -84,10 +127,35 @@ export class gameComponent implements OnInit{
   createKeyboard(){
     let keyboardContainer = document.getElementById("keyboard")
     this.letters.forEach(letter => {
-      let letterObj = document.createElement("mat-button");
-      letterObj.innerHTML = letter;
-      letterObj.setAttribute("id",letter);
-      keyboardContainer?.appendChild(letterObj);
+      //Buttons atributes of the keyboard and event function
+      let key = document.createElement("mat-button");
+      key.innerHTML = letter;
+      key.setAttribute("id","key-"+letter);
+      key.addEventListener("click",evt=>{
+        let idKey = (evt.target as HTMLElement).id;        
+        this.pressKeyButton(idKey);
+      });
+      keyboardContainer?.appendChild(key);
     });
   }
+
+  pressKeyButton(idKey:string){
+    let keyLetter = idKey[idKey.length-1]
+    let found = false
+    for(let i = 0; i < (this.actualWord).length; i++){
+      //Normalize method that replaces vowels with accent for the actual letter 
+      let wordGameLetter = (this.actualWord[i]).normalize("NFD").replace(/[\u0300-\u036f]/g, "");      
+      if(keyLetter===wordGameLetter){ //If the keyPressed and some actual word letter is the same, this control catches it        
+        const hiddenLetter = document.getElementById("gl-"+i+this.actualWord[i]) as HTMLElement;
+        hiddenLetter.innerHTML = this.actualWord[i]
+        found = true
+      }
+    }
+    console.log(keyLetter);
+    if (!found){
+      this.hangedStatus++
+      this.updateHangedStatus()
+    }
+  }
+  
 }
